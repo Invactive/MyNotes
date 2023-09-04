58dfdb46bf014a9eae00d43b4fe5fbd8736f6387
Invactive <gkuba8@gmail.com>
2023-09-04 21:14:21 +0200

    Added delete note feature

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -3,6 +3,8 @@ import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/enums/menu_action.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/services/crud/notes_service.dart';
+import 'package:mynotes/utilities/dialogs/logout_dialog.dart';
+import 'package:mynotes/views/notes/notes_list_view.dart';
 
 class NotesView extends StatefulWidget {
   const NotesView({super.key});
@@ -72,19 +74,12 @@ class _NotesViewState extends State<NotesView> {
                             if (snapshot.hasData) {
                               final allNotes =
                                   snapshot.data as List<DatabaseNote>;
-                              return ListView.builder(
-                                  itemCount: allNotes.length,
-                                  itemBuilder: (context, index) {
-                                    final note = allNotes[index];
-                                    return ListTile(
-                                      title: Text(
-                                        note.text,
-                                        maxLines: 1,
-                                        softWrap: true,
-                                        overflow: TextOverflow.ellipsis,
-                                      ),
-                                    );
-                                  });
+                              return NotesListView(
+                                notes: allNotes,
+                                onDeleteNote: (note) async {
+                                  await _notesService.deleteNote(id: note.id);
+                                },
+                              );
                             } else {
                               return const CircularProgressIndicator();
                             }
@@ -98,29 +93,4 @@ class _NotesViewState extends State<NotesView> {
               }
             }));
   }
-}
-
-Future<bool> showLogOutDialog(BuildContext context) {
-  return showDialog(
-    context: context,
-    builder: (context) {
-      return AlertDialog(
-        title: const Text("Sign out"),
-        content: const Text("Are you sure you want to sign out?"),
-        actions: [
-          TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(false);
-              },
-              child: const Text("Cancel")),
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(true);
-            },
-            child: const Text("Log out"),
-          )
-        ],
-      );
-    },
-  ).then((value) => value ?? false);
 }
